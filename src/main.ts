2b3d8c6e3437a966c594c06f1d4272dfd844a1a7
Malley <jmalley@goempyrean.com>
2024-12-10 10:09:03 -0600

    remove all the crap

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 const app = createApp(App)
+
+const http = axios.create({
+  baseURL: 'http://localhost:8055',
+  timeout: 10000,
+})
+
+app.provide('http', http)
 
 app.use(router)
 
